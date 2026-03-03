1078a649c9019189a3e527568384041ded0878ae
Lucas Baptista <lucasbaptista2106@gmail.com>
2026-03-02 21:08:41 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+alert("Boas vindas meu caro");
+let corCachorro = "Preto";
+let chute = prompt("Qual a cor do meu cachorro? Preto ou Branco?")
+
+if (chute == corCachorro) {
+    alert("Parabéns, você acertou a cor do meu cachorro!")  
+} else {
+    alert("Ops, você errou a cor do meu cachorro. Tente novamente!")
+}
